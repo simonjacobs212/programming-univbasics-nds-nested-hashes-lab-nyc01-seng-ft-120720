a9df41c14717123df0d9336e65f2d619bd17171d
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-18 17:39:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,11 +14,7 @@ end
 # Array. Ruby constants are denoted by ALL_CAPS
 
 def literal_aoh
-  new_arr = [{ name:  "Don Gately", occupation: "Live-in Staff" },
-  JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
-  KATE_G = { name:  "Kate Gompert", occupation: "None" },
-  BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }]
+  new_arr = [{ name:  "Don Gately", occupation: "Live-in Staff" }, { name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" }, { name:  "Bruce Green", occupation: "Fan of Mildred" }]
 end
 
 # Using Array literal syntax only, build a nested array that uses the data in
