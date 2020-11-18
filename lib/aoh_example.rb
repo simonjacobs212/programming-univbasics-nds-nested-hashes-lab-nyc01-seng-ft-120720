76543b9eca37a147d037c9e41677a2b28fd46dcf
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-11-18 17:34:08 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  names = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 # Build an array that contains (or, "nests") the constants into a single
